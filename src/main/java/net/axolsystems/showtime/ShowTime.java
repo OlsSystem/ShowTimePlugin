557cb081064425d4ce6d6b168dacac60662485f7
OlsSystem <70857587+OlsSystem@users.noreply.github.com>
2024-12-25 12:28:38 +0000

    Started On Scoreboard

--- src/main/java/net/axolsystems/showtime/ShowTime.java
+++ src/main/java/net/axolsystems/showtime/ShowTime.java
@@ -3,6 +3,8 @@ package net.axolsystems.showtime;
 import java.io.File;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.ScoreboardManager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -13,6 +15,7 @@ import org.bukkit.Location;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Boat;
 import org.bukkit.entity.Player;
 import org.bukkit.util.Vector;
@@ -27,6 +30,11 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
+import org.bukkit.scoreboard.DisplaySlot;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Score;
+import org.bukkit.scoreboard.Team;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import net.luckperms.api.LuckPerms;
@@ -34,7 +42,6 @@ import net.luckperms.api.LuckPermsProvider;
 import net.luckperms.api.model.user.User;
 import net.luckperms.api.model.user.UserManager;
 import net.luckperms.api.query.QueryOptions;
-import net.md_5.bungee.api.ChatColor;
 
 public class ShowTime extends JavaPlugin implements Listener {
 
@@ -77,7 +84,7 @@ public class ShowTime extends JavaPlugin implements Listener {
 
         ShowTimeUpdate.getInstance().autoUpdate();
 
-        loadLaunchers();
+        //loadLaunchers();
         initializeTeams();
         updateTabList();
 
@@ -90,7 +97,7 @@ public class ShowTime extends JavaPlugin implements Listener {
 
     @Override
     public void onDisable() {
-        saveLaunchers();
+        //saveLaunchers();
         logger.log(Level.WARNING, "ShowTime v{0} is disabling.", version);
     }
 
@@ -215,10 +222,175 @@ public class ShowTime extends JavaPlugin implements Listener {
 
     public void updateTabList() {
         for (Player player : Bukkit.getOnlinePlayers()) {
-            String header = ChatColor.GREEN.toString() + ChatColor.BOLD + "PROJECT SHOWTIME\n" + ChatColor.YELLOW + "Presented By " + ChatColor.DARK_AQUA + "OlsSystem" + ChatColor.YELLOW + "&" + ChatColor.AQUA + "SkwSliice\n" + ChatColor.RED + "---------------------------\n" + ChatColor.WHITE + "EVENT STATISTICS:" + "\n\n" + getTeamData();
+            //String header = ChatColor.GREEN.toString() + ChatColor.BOLD + "PROJECT SHOWTIME\n" + ChatColor.YELLOW + "Presented By " + ChatColor.DARK_AQUA + "OlsSystem" + ChatColor.YELLOW + "&" + ChatColor.AQUA + "SkwSliice\n" + ChatColor.RED + "---------------------------\n" + ChatColor.WHITE + "EVENT STATISTICS:" + "\n\n" + getTeamData();
 
-            player.setPlayerListHeader(header);
+            //player.setPlayerListHeader(header);
         }
     }
- 
+
+    private void updateScoreboard(String username) {
+        
+        JSONObject teamData = ShowTimeAPI.apiGetMethod("/api/teamdata");
+        JSONObject settingsData = ShowTimeAPI.apiGetMethod("/api/getSettings");
+        JSONObject settings = settingsData.getJSONObject("system");
+        JSONArray teams = teamData.getJSONArray("teams");
+        JSONArray pointsArray = settingsData.getJSONArray("pointsArray");
+        
+        Boolean inRound = settings.getBoolean("inRound");
+
+        Player player = Bukkit.getPlayer(username);
+        if (player == null) {
+            logger.log(Level.INFO, "Player with the username {0} is not online.", username);
+            return;
+        }
+
+        ScoreboardManager manager = Bukkit.getScoreboardManager();
+        if (manager == null) {
+            logger.log(Level.INFO, "Soreboard manager is not avaliable currently.");
+        }
+
+        Scoreboard playerScoreboard = manager.getNewScoreboard();
+        Objective objective = playerScoreboard.registerNewObjective("scoreboard", "dummy", ChatColor.GOLD + "Scoreboard");
+
+        if (inRound) {
+
+            objective.setDisplaySlot(DisplaySlot.SIDEBAR);
+            objective.setDisplayName(ChatColor.RED.toString() + ChatColor.BOLD  + "Project Showtime Season 1");
+
+            Score divider = objective.getScore(ChatColor.DARK_GRAY + "---------------------------");
+            divider.setScore(16);
+
+            Score gameInfo = objective.getScore(ChatColor.YELLOW + "Game: " + ChatColor.WHITE + settings.getInt("gamePos") + "/6: " + settings.getString("gameName"));
+            gameInfo.setScore(15);
+            Score roundInfo = objective.getScore(ChatColor.YELLOW + "Round: " + ChatColor.WHITE + settings.getInt("roundNumber") + "/" + settings.getInt("maxRounds"));
+            roundInfo.setScore(14);
+            Score mapInfo = objective.getScore(ChatColor.YELLOW + "Map: " + ChatColor.WHITE + settings.getString("mapName"));
+            mapInfo.setScore(13);
+
+            List<JSONObject> teamList  = new ArrayList<>();
+            for (int i = 0; i < pointsArray.length(); i++) {
+                JSONObject teamPoint = pointsArray.getJSONObject(i);
+                String teamName = teamPoint.getString("team");
+                int points = teamPoint.getInt("points");
+
+                for (int j = 0; j < teams.length(); j++) {
+                    JSONObject team = teams.getJSONObject(j);
+
+                    if (team.getString("teamName").equals(teamName)) {
+                        team.put("points", points);
+                        teamList.add(team);
+                        break;
+                    }
+                }
+            }
+
+            teamList.sort((a, b) -> Integer.compare(b.getInt("points"), a.getInt("points")));
+
+            for (int i = 0; i < Math.min(teamList.size(), 3); i++) {
+                JSONObject team = teamList.get(i);
+                String teamName = team.getString("teamName");
+                int score = team.getInt("points");
+
+                Score scoreEntry = objective.getScore((i + 1) + getPositionSuffix(i + 1) + ": " + teamName + " - " + score);
+                scoreEntry.setScore(11 - i);
+            }
+
+            Score header = objective.getScore(ChatColor.DARK_GRAY + "---------------------------");
+            header.setScore(8);
+
+            boolean userFound = false;
+
+            for (int i = 0; i < teamList.size(); i++) {
+                JSONObject team = teamList.get(i);
+                JSONArray players = team.getJSONArray("players");
+
+                for (int j = 0; j < players.length(); j++) {
+                    if (players.getString(j).equals(username)) {
+                        String positionSuffix = getPositionSuffix(i + 1);
+                        Score userTeamPosScore = objective.getScore((i + 1) + positionSuffix + ": " + team.getString("teamName") + " - " + team.getInt("points") + " ");
+                        userTeamPosScore.setScore(7);
+                        userFound = true;
+                        break;
+                    }
+                }
+                if (userFound) {
+                    break;
+                }
+            }
+
+            player.setScoreboard(playerScoreboard);
+        } else {
+
+            objective.setDisplaySlot(DisplaySlot.SIDEBAR);
+            objective.setDisplayName(ChatColor.RED.toString() + ChatColor.BOLD + "Project Showtime Season1");
+
+            boolean userFound = false;
+
+            for (int i = 0; i < teams.length(); i++) {
+                JSONObject team = teams.getJSONObject(i);
+                String teamName = team.getString("teamName");
+                String colour = team.getString("color");
+                int score = team.getInt("points");
+                String lpGroup = team.getString("lpGroup");
+                JSONArray players = team.getJSONArray("players");
+
+                for (int j = 0; j < players.length(); j++) {
+                    if (players.getString(j).equals(username)) {
+                        userFound = true;
+                        String teamNameWithColor = colour + "&l" + teamName;
+
+                        Score header = objective.getScore(ChatColor.BOLD + "== Your Team ==");
+                        header.setScore(10);
+                        String teamEntry = ChatColor.translateAlternateColorCodes('&', teamNameWithColor);
+                        Score teamNameView = objective.getScore(teamEntry);
+                        teamNameView.setScore(1);
+                        String scoreEntry = ChatColor.WHITE + "Score: " + ChatColor.GOLD + score;
+                        Score teamScoreView = objective.getScore(scoreEntry);
+                        teamScoreView.setScore(0);
+
+                        Team minecraftTeam = playerScoreboard.getTeam(lpGroup);
+                        if (minecraftTeam == null) {
+                            minecraftTeam = playerScoreboard.registerNewTeam(lpGroup);
+                            minecraftTeam.setDisplayName(teamName);
+                            minecraftTeam.setColor(getChatColor(colour));
+                            logger.log(Level.WARNING, "Created new minecraft team for {0}", teamName);
+                        }
+                        minecraftTeam.addEntry(player.getName());
+                    
+                    }
+                }
+            }
+
+            if (!userFound) {
+
+                logger.log(Level.INFO, "User {0}is not appart of any team.", username);
+
+                Score header = objective.getScore(ChatColor.BOLD + "== Your Rank ==");
+                header.setScore(10);
+                Score noTeamScore = objective.getScore(ChatColor.GOLD.toString() + ChatColor.BOLD + getPlayerPrefix(player));
+                noTeamScore.setScore(1);
+            }
+
+            int playerCount = 0;
+            for (int i = 0; i < teams.length(); i++) {
+                JSONObject team = teams.getJSONObject(i);
+                JSONArray players = team.getJSONArray("players");
+                for (int j = 0; j < players.length(); j++) {
+                    String playerName = players.getString(j);
+                    for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
+                        if (onlinePlayer.getName().equals(playerName)) {
+                            playerCount++;
+                        }
+                    }
+                }
+            }
+
+            Score spacer = objective.getScore(" ");
+            spacer.setScore(-1);
+            Score playersEntry = objective.getScore("Online Players: " + ChatColor.WHITE + playerCount + "/32");
+            playersEntry.setScore(-2);
+
+            player.setScoreboard(playerScoreboard);
+        }
+    }
 }
